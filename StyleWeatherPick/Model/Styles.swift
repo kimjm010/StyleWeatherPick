23b5be662e80334b21ba7d4d6e1d11a42e5384f7
Chris Kim <kimjm010@icloud.com>
2022-12-20 01:34:04 +0900

    [StyleWeatherPick - Add]
    - Add custom font
    - Add styles categories

---
+++ StyleWeatherPick/Model/Styles.swift
@@ -0,0 +1,28 @@
+//
+//  Styles.swift
+//  StyleWeatherPick
+//
+//  Created by Chris Kim on 12/20/22.
+//
+
+import Foundation
+
+enum Styles {
+    enum MenStyles {
+        case Street
+        case Hiphop
+        case Casual
+        case Dandy
+        case Classic
+        case Minimalism
+    }
+    
+    enum WomenStyles {
+        case Street
+        case Hiphop
+        case Casual
+        case BusinessCasual
+        case Feminime
+        case Minimalism
+    }
+}
